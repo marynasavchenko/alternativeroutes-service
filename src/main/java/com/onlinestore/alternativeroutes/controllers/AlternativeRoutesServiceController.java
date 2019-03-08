f3bf3bbffcab425f7bf708fc6922cea4d6a8ae69
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-08 09:15:57 +0100

    added getRouteRecord()

--- src/main/java/com/onlinestore/alternativeroutes/controllers/AlternativeRoutesServiceController.java
+++ src/main/java/com/onlinestore/alternativeroutes/controllers/AlternativeRoutesServiceController.java
@@ -1,5 +1,10 @@
 package com.onlinestore.alternativeroutes.controllers;
 
+import com.onlinestore.alternativeroutes.model.RouteRecord;
+import com.onlinestore.alternativeroutes.services.RouteRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/v1/route")
 public class AlternativeRoutesServiceController {
 
+	private RouteRecordService routeRecordService;
+
+	@Autowired
+	public AlternativeRoutesServiceController(RouteRecordService routeRecordService) {
+		this.routeRecordService = routeRecordService;
+	}
+
+	@GetMapping(value = "/records/{serviceName}")
+	public RouteRecord getRouteRecord(@PathVariable("serviceName") String serviceName) {
+		return routeRecordService.getRouteRecordByServiceName(serviceName);
+	}
 
 }
