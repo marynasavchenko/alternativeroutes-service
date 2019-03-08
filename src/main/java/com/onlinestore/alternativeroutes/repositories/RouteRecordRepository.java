72f243a05858bed3f82a1c6bfb6cc542735789a7
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-08 09:10:10 +0100

    added missed @Repository

--- src/main/java/com/onlinestore/alternativeroutes/repositories/RouteRecordRepository.java
+++ src/main/java/com/onlinestore/alternativeroutes/repositories/RouteRecordRepository.java
@@ -2,7 +2,9 @@ package com.onlinestore.alternativeroutes.repositories;
 
 import com.onlinestore.alternativeroutes.model.RouteRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RouteRecordRepository extends JpaRepository<RouteRecord, String> {
 
 	RouteRecord findByServiceName(String serviceName);
