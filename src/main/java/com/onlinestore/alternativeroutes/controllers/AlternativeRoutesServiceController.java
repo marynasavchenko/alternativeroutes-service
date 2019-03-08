55323a1de5d19dcad85819362e684e66997ba5ec
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-08 08:39:26 +0100

    added AlternativeRoutesServiceController

---
+++ src/main/java/com/onlinestore/alternativeroutes/controllers/AlternativeRoutesServiceController.java
@@ -0,0 +1,11 @@
+package com.onlinestore.alternativeroutes.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/route")
+public class AlternativeRoutesServiceController {
+
+
+}
