de427cffa724c2ca4fb4b882d728ecffa57870d2
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-12 08:42:02 +0100

    added CustomHystrixConcurrencyStrategy

---
+++ src/main/java/com/onlinestore/alternativeroutes/hystrix/CustomHystrixConcurrencyStrategy.java
@@ -0,0 +1,42 @@
+package com.onlinestore.alternativeroutes.hystrix;
+
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariable;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariableLifecycle;
+import com.netflix.hystrix.strategy.properties.HystrixProperty;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class CustomHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy {
+
+	private HystrixConcurrencyStrategy existingConcurrencyStrategy;
+
+	public CustomHystrixConcurrencyStrategy(HystrixConcurrencyStrategy existingConcurrencyStrategy) {
+		this.existingConcurrencyStrategy = existingConcurrencyStrategy;
+	}
+
+
+	@Override
+	public ThreadPoolExecutor getThreadPool(HystrixThreadPoolKey threadPoolKey, HystrixProperty<Integer> corePoolSize, HystrixProperty<Integer> maximumPoolSize, HystrixProperty<Integer> keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+		return super.getThreadPool(threadPoolKey, corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+	}
+
+	@Override
+	public BlockingQueue<Runnable> getBlockingQueue(int maxQueueSize) {
+		return super.getBlockingQueue(maxQueueSize);
+	}
+
+	@Override
+	public <T> Callable<T> wrapCallable(Callable<T> callable) {
+		return super.wrapCallable(callable);
+	}
+
+	@Override
+	public <T> HystrixRequestVariable<T> getRequestVariable(HystrixRequestVariableLifecycle<T> rv) {
+		return super.getRequestVariable(rv);
+	}
+}
