c4c4f2834b78018260dc4f63fe6aef655ba15b5b
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-12 08:28:50 +0100

    added ThreadLocalConfiguration

---
+++ src/main/java/com/onlinestore/alternativeroutes/hystrix/ThreadLocalConfiguration.java
@@ -0,0 +1,15 @@
+package com.onlinestore.alternativeroutes.hystrix;
+
+import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ThreadLocalConfiguration {
+
+	private HystrixConcurrencyStrategy existingConcurrencyStrategy;
+
+	@Autowired(required = false)
+	public ThreadLocalConfiguration(HystrixConcurrencyStrategy existingConcurrencyStrategy) {
+		this.existingConcurrencyStrategy = existingConcurrencyStrategy;
+	}
+
+}
