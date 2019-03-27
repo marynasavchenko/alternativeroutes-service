9a86881737b37a8e404383f399b6bb9b74f6e1c3
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-27 12:45:16 +0100

    added doc comments to RouteRecordRepository

--- src/main/java/com/onlinestore/alternativeroutes/repositories/RouteRecordRepository.java
+++ src/main/java/com/onlinestore/alternativeroutes/repositories/RouteRecordRepository.java
@@ -4,8 +4,16 @@ import com.onlinestore.alternativeroutes.domain.RouteRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Spring Data repository for {@link RouteRecord} class.
+ */
 @Repository
 public interface RouteRecordRepository extends JpaRepository<RouteRecord, String> {
-
+	/**
+	 * Looks up route record of the specified service in the database.
+	 *
+	 * @param serviceName name of the service
+	 * @return route record
+	 */
 	RouteRecord findByServiceName(String serviceName);
 }
