09146619681b589142ff2ef3e4304367279a52c8
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-27 12:25:23 +0100

    added doc comments to AlternativeRoutesServiceController

--- src/main/java/com/onlinestore/alternativeroutes/controllers/AlternativeRoutesServiceController.java
+++ src/main/java/com/onlinestore/alternativeroutes/controllers/AlternativeRoutesServiceController.java
@@ -8,17 +8,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Rest controller for managing route's records.
+ */
 @RestController
 @RequestMapping(value = "/v1/route")
 public class AlternativeRoutesServiceController {
-
+	/**
+	 * Service that supports opertions with route records.
+	 */
 	private RouteRecordService routeRecordService;
 
+	/**
+	 * Constructs a new {@code AlternativeRoutesServiceController} instance.
+	 *
+	 * @param routeRecordService
+	 */
 	@Autowired
 	public AlternativeRoutesServiceController(RouteRecordService routeRecordService) {
 		this.routeRecordService = routeRecordService;
 	}
 
+	/**
+	 * GET  /records/{serviceName} : gets route record of specified service.
+	 *
+	 * @param serviceName name of the service
+	 * @return route record
+	 */
 	@GetMapping(value = "/records/{serviceName}")
 	public RouteRecord getRouteRecord(@PathVariable("serviceName") String serviceName) {
 		return routeRecordService.getRouteRecordByServiceName(serviceName);
