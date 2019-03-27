99a692922ebd08ff8575e2367f593b9b51f68a48
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-27 13:03:35 +0100

    added doc comments to RouteRecordService

--- src/main/java/com/onlinestore/alternativeroutes/services/RouteRecordService.java
+++ src/main/java/com/onlinestore/alternativeroutes/services/RouteRecordService.java
@@ -6,16 +6,32 @@ import com.onlinestore.alternativeroutes.repositories.RouteRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service that supports operations for managing route's records.
+ */
 @Service
 public class RouteRecordService {
-
+	/**
+	 * Spring Data repository for {@link RouteRecord}.
+	 */
 	private RouteRecordRepository routeRecordRepository;
 
+	/**
+	 * Constructs new {@code RouteRecordService} instance.
+	 *
+	 * @param routeRecordRepository Spring Data repository
+	 */
 	@Autowired
 	public RouteRecordService(RouteRecordRepository routeRecordRepository) {
 		this.routeRecordRepository = routeRecordRepository;
 	}
 
+	/**
+	 * Gets route record of the specified service.
+	 *
+	 * @param serviceName name of the service
+	 * @return route record
+	 */
 	public RouteRecord getRouteRecordByServiceName(String serviceName) {
 		RouteRecord routeRecord = routeRecordRepository.findByServiceName(serviceName);
 		if (routeRecord == null) throw new RouteRecordNotFoundException();
