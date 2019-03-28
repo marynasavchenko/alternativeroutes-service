0ea2130a545b13052b2844993796b9423b1a9379
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-28 08:31:18 +0100

    added doc comments to Application

--- src/main/java/com/onlinestore/alternativeroutes/Application.java
+++ src/main/java/com/onlinestore/alternativeroutes/Application.java
@@ -5,11 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Class that bootstraps this service.
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
 public class Application {
-
+	/**
+	 * Main method, used to run this application.
+	 *
+	 * @param args the string array, that contains command line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
