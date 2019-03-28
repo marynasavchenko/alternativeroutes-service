e9ab70d398419da707d70634026695ff6d53a6b2
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-28 08:46:40 +0100

    added doc comments to RouteRecordNotFoundException

--- src/main/java/com/onlinestore/alternativeroutes/exceptions/RouteRecordNotFoundException.java
+++ src/main/java/com/onlinestore/alternativeroutes/exceptions/RouteRecordNotFoundException.java
@@ -3,6 +3,9 @@ package com.onlinestore.alternativeroutes.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when route record of the service can not be found.
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class RouteRecordNotFoundException extends RuntimeException {
 }
