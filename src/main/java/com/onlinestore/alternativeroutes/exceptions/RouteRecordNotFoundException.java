fd8070c3d8e7e36c1e8658977ce18feca8ae9858
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-04 11:17:37 +0200

    added constructor to RouteRecordNotFoundException

--- src/main/java/com/onlinestore/alternativeroutes/exceptions/RouteRecordNotFoundException.java
+++ src/main/java/com/onlinestore/alternativeroutes/exceptions/RouteRecordNotFoundException.java
@@ -8,4 +8,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class RouteRecordNotFoundException extends RuntimeException {
+	/**
+	 * Constructs a new {@code RouteRecordNotFoundException} instance.
+	 *
+	 * @param serviceId service id
+	 */
+	public RouteRecordNotFoundException(String serviceId) {
+		super("RouteRecord of service " + serviceId + "not found");
+	}
 }
