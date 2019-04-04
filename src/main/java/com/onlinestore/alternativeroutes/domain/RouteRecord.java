64c305baab3e37dce9e69338faffe514df89b274
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-04 11:38:45 +0200

    added hashcode and equals

--- src/main/java/com/onlinestore/alternativeroutes/domain/RouteRecord.java
+++ src/main/java/com/onlinestore/alternativeroutes/domain/RouteRecord.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 /**
  * Route record entity class.
@@ -88,4 +89,16 @@ public class RouteRecord {
 				'}';
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		RouteRecord that = (RouteRecord) o;
+		return Objects.equals(serviceName, that.serviceName);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(serviceName);
+	}
 }
