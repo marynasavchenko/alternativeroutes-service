59fda156dadeb21be115143a4541ddff9ba3a49f
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-26 15:50:07 +0200

    formatted URI

--- src/test/java/com/onlinestore/alternativeroutes/controllers/AlternativeRoutesServiceControllerTest.java
+++ src/test/java/com/onlinestore/alternativeroutes/controllers/AlternativeRoutesServiceControllerTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AlternativeRoutesServiceControllerTest {
 
 	private static final String SERVICE_NAME = "customerservice";
-	private static final String URI = "/v1/route/records/customerservice";
+	private static final String URI = "/v1/route/records";
 
 	@MockBean
 	private RouteRecordService routeRecordService;
@@ -31,7 +31,7 @@ public class AlternativeRoutesServiceControllerTest {
 
 	@Test
 	public void shouldGetRouteRecordByServiceName() throws Exception {
-		mockMvc.perform(get(URI)).andExpect((status().isOk()));
+		mockMvc.perform(get(URI + "/" + SERVICE_NAME)).andExpect((status().isOk()));
 		verify(routeRecordService).getRouteRecordByServiceName(SERVICE_NAME);
 	}
 
