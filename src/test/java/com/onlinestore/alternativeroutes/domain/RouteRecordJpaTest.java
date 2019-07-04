37acecc26ba19530e6769b59451a348e79491f3e
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-07-04 16:58:50 +0200

    added constants

--- src/test/java/com/onlinestore/alternativeroutes/domain/RouteRecordJpaTest.java
+++ src/test/java/com/onlinestore/alternativeroutes/domain/RouteRecordJpaTest.java
@@ -13,13 +13,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @DataJpaTest
 public class RouteRecordJpaTest {
 	private static final String SERVICE_NAME = "customerservice";
+	private static final String ACTIVE_STATUS = "YES";
+	private static final String ENDPOINT = "";
+	private static final int WEIGHT = 1;
 
 	@Autowired
 	private TestEntityManager entityManager;
 
 	@Test
 	public void shouldMapRouteRecordEntity() throws Exception {
-		RouteRecord routeRecord = this.entityManager.persistAndFlush(new RouteRecord(SERVICE_NAME, "YES", "", 1));
+		RouteRecord routeRecord = this.entityManager.persistAndFlush(new RouteRecord(SERVICE_NAME, ACTIVE_STATUS, ENDPOINT, WEIGHT));
 		Assertions.assertThat(routeRecord.getServiceName()).isNotNull();
 	}
 }
